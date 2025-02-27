a421de117823cfe48f3b8e7f0d64d9c1c45da4ac
Ali Kooshesh <alikooshesh007@gmail.com>
2025-02-27 11:54:34 +0330

    feat(todoReducer): create remove action

--- src/redux/reducers/todoReducer.ts
+++ src/redux/reducers/todoReducer.ts
@@ -1,22 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Itodo } from "../../interfeaces/todo";
 
+const init : Itodo[] = []
+
+
 const todoReducer = createSlice({
     name : "todos",
-    initialState : [
-        {
-            id : 1,
-            title : "test"
-        }
-    ] as Itodo[],
+    initialState : init,
     reducers : {
         add : (state,action)=>{
             console.log(action)
             state.push({...action.payload})
+        },
+        remove : (state,action)=>{
+            return state.filter(item => item.id !== action.payload.id)
         }
     }
 })
 
 
-export const {add} = todoReducer.actions
+export const {add , remove} = todoReducer.actions
 export default todoReducer.reducer
